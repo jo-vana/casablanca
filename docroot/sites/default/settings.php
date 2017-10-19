b6cee561b0097fc202c794de5c03b0739981141d
vanja <jovana.nukic@canicinteractive.com>
2017-10-19 13:05:09 +0200

    Settings and new modules

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -12,3 +12,6 @@ $databases['default']['default'] = array (
 $settings['hash_salt'] = 'iTDVk72OBcPE14xFzgcl28ai4sWBmWeuCCl8ftfrT3Mg1raFrCSp7inZY_RFyaLwz0iy5eWUoA';
 $settings['install_profile'] = 'standard';
 $config_directories['sync'] = 'sites/default/files/config__f0vk2SGjhLlfW_l-907iHf7o7hv2nn5sQzE8eT7eZk7fMWwu-w8e-49xopXdEw3KyMx5RbyLQ/sync';
+$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
+$settings['cache']['bins']['render'] = 'cache.backend.null';
+$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
